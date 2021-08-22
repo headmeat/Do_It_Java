6bed6a0eb7a458fab006fb4f60f14b31be27ad36
headmeat <headmeat@naver.com>
2021-08-23 01:47:10 +0900

    Create 8&9backup.java

---
+++ 5장/8&9backup.java
@@ -0,0 +1,102 @@
+package john.john;
+
+import java.util.Arrays;
+
+public class Queen {
+	static int[] pos = new int[8];
+	static boolean[] flag_a = new boolean[8], flag_b = new boolean[15], flag_c = new boolean[15];
+	
+	static void print() {
+		for(int i=0;i<pos.length;i++) {//one row
+			for(int j=0;j<pos.length;j++) {//for each col
+				if(pos[j] == i) System.out.printf("%2s", "■");
+				else System.out.printf("%2s", "□");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+
+	static void set(int i) {
+		for(int j=0;j<8;j++) {
+			if(flag_a[j] == false && flag_b[i+j] == false && flag_c[i-j+7] == false) {
+				pos[i] = j;
+				if(i==7) {
+					print();
+				}else {
+					flag_a[j] = flag_b[i+j] = flag_c[i-j+7] = true;
+					set(i+1);
+					flag_a[j] = flag_b[i+j] = flag_c[i-j+7] = false;
+				}
+			}
+		}
+	}
+
+	static int check = 0;
+	static int[] sw = new int[8];//pos[i]의 현재까지 계산했던 j 인덱스 상태 정보
+	static int bwfw = 0;//0: 드가자, 1: 뒤로 빼고 이어서 해라
+	
+	static void set1(int i) {
+		while(true) {
+			for(sw[i] = (bwfw==0)?0:sw[i];sw[i]<8;sw[i]++) {
+				/*
+				if(pos[0]==1 && pos[1]==3 && i==3) {
+					System.out.println("섹스");
+					System.out.println(sw[i]);
+					System.out.println(flag_a[sw[i]]);
+				}*/
+				
+				if(flag_a[sw[i]] == false && flag_b[i+sw[i]] 
+						== false && flag_c[i-sw[i]+7] == false) {
+					pos[i] = sw[i];
+					
+					if(i==7) {
+						print();
+						i -= 1;
+						flag_a[sw[i]] = flag_b[i+sw[i]] = flag_c[i-sw[i]+7] = false;
+						break;
+					}else {
+						/*
+						if(pos[0]==1 && pos[1]==3) {
+							System.out.println("씨발");
+							System.out.println(i);
+							print();
+						}*/
+						flag_a[sw[i]] = flag_b[i+sw[i]] = flag_c[i-sw[i]+7] = true;
+						bwfw = 0;i += 1;sw[i] = -1;
+					}
+				}
+			}
+			
+			//깨진 거임 for loop가.
+
+			bwfw = 1;i -= 1;
+			if(i==-1) return;
+			flag_a[sw[i]] = flag_b[i+sw[i]] = flag_c[i-sw[i]+7] = false;
+			sw[i] += 1;
+			/*
+			if(i==0) {
+				System.out.println(i+" "+sw[i]);
+				System.out.println(Arrays.toString(flag_a));
+				System.out.println(Arrays.toString(flag_b));
+				System.out.println(Arrays.toString(flag_c));
+				for(int k=1;k<pos.length;k++) {
+					pos[k] = -1;
+				}
+			}*/
+
+
+			/*
+			i -= 1;//i번째 인덱스를 다 돌았기 때문에
+			flag_a[sw[i]] = flag_b[i+sw[i]] = flag_c[i-sw[i]+7] = false;
+			*/
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("START");
+		set1(0);
+		System.out.println("FINISH");
+	}
+}
