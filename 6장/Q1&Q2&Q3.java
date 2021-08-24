fc0d7b37a7b8d863661cd4ced8261c5f32205fa8
headmeat <headmeat@naver.com>
2021-08-25 02:35:33 +0900

    Update and rename Q1&Q2.java to Q1&Q2&Q3.java

--- 6장/Q1&Q2&Q3.java
+++ 6장/Q1&Q2&Q3.java
@@ -24,21 +24,32 @@ class BubbleSort {
 	
 	static void sort(int[] arr) {
 		int len = arr.length;
+		int swap_cnt = 0,  cnt = 0;
 		
 		for(int i=0;i<len-1;i++) {
-			for(int j=0;j<len-1;j++) {
+			int prev = swap_cnt;
+			for(int j=i;j<len-1;j++) {
+				cnt += 1;
 				if(arr[j+1]<arr[j]) {
+					swap_cnt += 1;
 					printArr(arr, j, true);
 					swap(arr, j+1, j);
 				}else {
 					printArr(arr, j, false);
 				}
 			}
+			
+			if(prev==swap_cnt) break;
 		}
+		
+		System.out.println("\n총 "+cnt+"회 실행됐습니다.\nSwap은 총 "+swap_cnt+"회 실행됐습니다.\n");
 	}
 	
 	public static void main(String[] args) {
-		int[] arr = new int[] {1,54,6,1,2,4512,55,6213};
+		int[] arr = new int[] {1,54,6,1,2,55,56};
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println("버블 정렬을 수행합니다.\n");
 		
 		sort(arr);
 		
